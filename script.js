f74c1e81f67ac614b5bb228e82fc14fc0fd4efa8
vano31 <vanosike31@gmail.com>
2022-12-02 18:12:22 -0500

    Complete boilerplate, add makeNode function

---
+++ script.js
@@ -0,0 +1,12 @@
+//Tic Tac Toe
+
+let makeNode = function(xvalue, yvalue) {
+    label = [xvalue, yvalue];
+    icon = "z";
+    connections = {};
+
+    return {xvalue, yvalue, label, icon, connections}
+
+
+}
+
