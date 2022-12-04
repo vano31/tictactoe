9561238ce1c007136d9e12c56795784e26ded265
vano31 <vanosike31@gmail.com>
2022-12-04 16:07:17 -0500

    Node connections in grid finalized accurately Node connections still need to be put in node.connections

--- script.js
+++ script.js
@@ -3,10 +3,55 @@
 let makeNode = function(xvalue, yvalue) {
     label = [xvalue, yvalue];
     icon = "z";
-    connections = {};
+    connections = [];
 
     return {xvalue, yvalue, label, icon, connections}
 
 
 }
+
+const Grid = function(xmax, ymax) {
+
+    let NodeArray = [];
+
+    for (let numberx = 0; numberx < xmax; numberx++) {
+
+        for (let numbery = 0; numbery < ymax; numbery++) {
+
+            newNode = makeNode(numberx, numbery);            
+            NodeArray.push(newNode);
+
+        }
+
+    }
+
+
+    for (let i = 0; i < NodeArray.length; i++) {
+
+        let currentNode = NodeArray[i];
+        //console.log(currentNode);
+        
+        let cxvalue = currentNode.xvalue;
+        //console.log(currentNode.xvalue);
+
+        let cyvalue = currentNode.yvalue;
+        //console.log(currentNode.yvalue);
+
+        
+        let connectionNodes = NodeArray.filter(node => (node.label.toString() !== currentNode.label.toString()) &&  (node.label.toString() === [cxvalue, cyvalue + 1].toString() || node.label.toString() === [cxvalue, cyvalue - 1].toString() || node.label.toString() === [cxvalue + 1, cyvalue].toString() || node.label.toString() === [cxvalue - 1, cyvalue].toString() || node.label.toString() === [cxvalue + 1, cyvalue + 1].toString() || node.label.toString() === [cxvalue - 1, cyvalue - 1].toString() || node.label.toString() === [cxvalue - 1, cyvalue + 1].toString() || node.label.toString() === [cxvalue + 1, cyvalue - 1].toString()));
+        console.log(connectionNodes);
+        currentNode[connections] = connectionNodes;
+
+    }
+
+    return NodeArray;
+
+
+};
+
+
+//nodearray filter must be done AFTER all nodes are generated
+//let connectedNodes = NodeArray.filter(node => node.label[0] === newNode.label[0] || node.label[0] === newNode.label[0] + 1 || node.label[0] === newNode.label[0] - 1 || node.label[1] === newNode.label[1] || node.label[1] === newNode.label[1] + 1 || node.label[1] === newNode.label[1] - 1);
+//newNode.connections = connectedNodes;
+
 
