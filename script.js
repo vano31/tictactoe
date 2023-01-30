d6d72d35597c8ce8adfe48daf89557abbf5e8e0f
vano31 <vanosike31@gmail.com>
2023-01-30 12:31:52 -0500

    Fix event listener to change tile character on DOM

--- script.js
+++ script.js
@@ -39,7 +39,70 @@ const Grid = function(xmax, ymax) {
 
     return NodeArray;
 
+    
+
 };
+
+let bbtiles = document.querySelectorAll(`.tile`);
+
+const tictactoe = function() {
+
+    let board = Grid(3,3);
+    let boardtiles = document.querySelectorAll('.tile');
+
+    boardtiles.forEach(tile => {
+        tile.addEventListener(`click`, function(e) {
+            let t = e.target;
+            if (t.classList.contains('unfilled')) {
+                console.log(t);
+                t.className = `tile filled-x`;
+            }
+        })
+    })
+
+    /*
+
+    boardtiles.onclick = function(e) {
+        let t = e.target;
+        if (t.classList.contains(`unfilled`)) {
+            console.log(t);
+            target.className = `tile filled-x`;
+
+
+        }
+    }
+
+    */
+
+    /*
+
+    let iconchanger = function() {
+        for(tile in tiles) {
+            if (tile.className === `tile unfilled`) {
+                tile.className === `tile filled-x`
+
+            }
+        }
+    }
+
+    */
+
+    //board.addEventListener('click', )
+
+
+    /*
+    console.log(board);
+    return board;
+    */
+    console.log(board, boardtiles);
+    return { board, boardtiles }
+    
+
+}
+
+let {board, boardtiles} = tictactoe();
+
+
 
 
 
